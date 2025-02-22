39ad07a7e57f4919542e47ce85a88ead43d3974f
Dimas Bayu Nugroho <193190491+DimasVSuper@users.noreply.github.com>
2025-02-22 13:46:58 +0700

    commit

--- function_filtering.php
+++ function_filtering.php
@@ -1,63 +1,55 @@
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Demo</title>
 </head>
+
 <body>
     <?php
-    $books = [
-        [
-            'name' => 'The Adventures of Huckleberry Finn',
-            'author' => 'Mark Twain',
-            'releaseYear' => 1884,
-            'purchaseUrl' => 'https://www.amazon.com/The-Adventures-Huckleberry-Finn-Twain/dp/1985234876'
-        ],
-        [
-            'name' => 'The Catcher in the Rye',
-            'author' => 'J.D. Salinger',
-            'releaseYear' => 1951,
-            'purchaseUrl' => 'https://www.amazon.com/The-Catcher-Rye-J-D-Salinger/dp/0316769487'
-        ],
-        [
-            'name' => 'The Great Gatsby',
-            'author' => 'F. Scott Fitzgerald',
-            'releaseYear' => 1925,
-            'purchaseUrl' => 'https://www.amazon.com/The-Great-Gatsby-F-Scott-Fitzgerald/dp/0743273567'
-        ],
-        [
-            'name' => 'The Martian',
-            'author' => 'Andy Weir',
-            'releaseYear' => 2011,
-            'purchaseUrl' => 'https://www.amazon.com/The-Martian-Andy-Weir/dp/0553418025'
-        ],
-        [
-            'name' => 'Hail Marry',
-            'author' => 'Andy Weir',
-            'releaseYear' => 2017,
-            'purchaseUrl' => 'https://www.amazon.com/Hail-Mary-Andy-Weir/dp/0593135202'
-        ]
-    ];
-    function filterByAuthor(){
-        return 'test';
-    };
+        $books = [
+            [
+                'name' => 'Do Androids Dream of Electric Sheep',
+                'author' => 'Philip K. Dick',
+                'releaseYear' => 1968,
+                'purchaseUrl' => 'http://example.com'
+            ],
+            [
+                'name' => 'Project Hail Mary',
+                'author' => 'Andy Weir',
+                'releaseYear' => 2021,
+                'purchaseUrl' => 'http://example.com'
+            ],
+            [
+                'name' => 'The Martian',
+                'author' => 'Andy Weir',
+                'releaseYear' => 2011,
+                'purchaseUrl' => 'http://example.com'
+            ],
+        ];
+
+        function filterByAuthor($books, $author)
+        {
+            $filteredBooks = [];
+
+            foreach ($books as $book) {
+                if ($book['author'] === $author) {
+                    $filteredBooks[] = $book;
+                }
+            }
+
+            return $filteredBooks;
+        }
     ?>
-    <h1>Books by Andy Weir</h1>
+
     <ul>
-    <?php foreach($books as $book): ?>
-        <?php if($book['author'] === 'Andy Weir'): ?>
+        <?php foreach (filterByAuthor($books, 'Philip K. Dick') as $book) : ?>
             <li>
                 <a href="<?= $book['purchaseUrl'] ?>">
-                    <?= $book['name'] ?> by <?= $book['author'] ?>
+                    <?= $book['name']; ?> (<?= $book['releaseYear'] ?>) - By <?= $book['author'] ?>
                 </a>
             </li>
-        <?php endif; ?>
-    <?php endforeach; ?>
+        <?php endforeach; ?>
     </ul>
-
-    <p>
-        <?= filterByAuthor(); ?>
-    </p>
 </body>
 </html>
