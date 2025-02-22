8aa4c5fdd02f4fcdca9e8215178992fb6d6c4667
Dimas Bayu Nugroho <193190491+DimasVSuper@users.noreply.github.com>
2025-02-22 10:59:11 +0700

    commit

---
+++ variable.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>variable</title>
+</head>
+<body>
+    <?php
+    $nama = "Dimas";
+    $umur = 17;
+    $status_menikah = false;
+
+    echo "Nama : $nama <br>";
+    echo "Umur : $umur <br>";
+    echo "Status Menikah : $status_menikah";
+    $greeting = "Hello";
+    $dunia = "World";
+    echo "$greeting $dunia"."!<br>";
+    echo $greeting . " Saya " . $nama . " umur ku adalah " . $umur . " tahun ";
+    ?>
+</body>
+</html>
